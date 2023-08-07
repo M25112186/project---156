92c14e343c6ed7668357b6fb8640f2926c022ca2
m25112186 <99669367+M25112186@users.noreply.github.com>
2023-08-07 20:41:19 +0530

    Add files via upload

---
+++ js/fish.js
@@ -0,0 +1,38 @@
+AFRAME.registerComponent("fish", {
+    init: function () {
+      for (var i = 1;i<= 10;i++) {
+        const id = `fish${i}`;
+        const posX = Math.random() * 100 + -50;
+        const posY = Math.random() * 5 + 5;
+        const posZ = Math.random() * 60 + -40;
+
+        const position = { x: posX, y: posY, z: posZ };
+        this.genrateFish(id, position);
+      }
+    },
+    createFish: function (id, position) {
+      const fishEntity = document.querySelector("#fishModels");
+      var fishEl = document.createElement("a-entity");
+  
+      fishEl.setAttribute("id", id);
+      fishEl.setAttribute("position", position);
+      fishEl.setAttribute("rotation", { x: 0, y: 180, z: 0 });
+      fishEl.setAttribute("scale", { x: 0.3, y: 0.3, z: 0.3 });
+      fishEl.setAttribute("gltf-model","./assets/fish/scene.gltf");
+  
+      
+
+      fishEl.setAttribute("animation", {
+        property: "position",
+        to: "80 9 -25",
+        loop: "true",
+        dur: 1000
+      });
+  
+      fishEl.setAttribute("animation-mixer",{});
+      fishEl.setAttribute("game-play", {
+        elementId: `#${id}`,
+      });
+      fishEntity.appendChild(fishEl);
+    }
+  })
